3218fc034c44f2a33cd5727439e3da1c06d4d702
Abhishek Malviya <malviya.abhishek853@gmail.com>
2022-07-06 11:13:37 +0530

    Update UserEntity.java

--- src/main/java/com/smb/entity/UserEntity.java
+++ src/main/java/com/smb/entity/UserEntity.java
@@ -1,5 +1,43 @@
 package com.smb.entity;
 
-public class UserEntity {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user")
+public class UserEntity {
+	private String id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+//	private List<String> following = new ArrayList<>();
+//	private List<String> follower = new ArrayList<>();
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
