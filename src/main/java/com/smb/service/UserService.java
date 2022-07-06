b0609da2626d39c320fbf47684da68fd188baadf
Abhishek Malviya <malviya.abhishek853@gmail.com>
2022-07-06 15:26:02 +0530

    Update UserService.java

--- src/main/java/com/smb/service/UserService.java
+++ src/main/java/com/smb/service/UserService.java
@@ -3,6 +3,9 @@ package com.smb.service;
 import com.smb.entity.UserEntity;
 import com.smb.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepo userRepo;
@@ -36,4 +39,10 @@ public class UserService {
             return responseObj;
         }
     }
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
