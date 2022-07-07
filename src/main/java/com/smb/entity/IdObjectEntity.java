4a38d1e1b18fceed0dff07cdfd362441290183bb
vedant-banerjee <vedantbanerjee91@gmail.com>
2022-07-07 11:26:27 +0530

    Post
    Post

---
+++ src/main/java/com/smb/entity/IdObjectEntity.java
@@ -0,0 +1,26 @@
+package com.smb.entity;
+
+public class IdObjectEntity {
+	private String id;
+
+	public IdObjectEntity(String id) {
+		super();
+		this.id = id;
+	}
+
+	public IdObjectEntity() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+}
+
--- src/main/java/com/smb/service/PostService.java
+++ src/main/java/com/smb/service/PostService.java
@@ -9,6 +9,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.smb.entity.DoubleIdObjectEntity;
+import com.smb.entity.IdObjectEntity;
 import com.smb.entity.PostEntity;
 import com.smb.entity.UserEntity;
 import com.smb.repo.PostRepo;
@@ -29,5 +31,101 @@ public class PostService {
         responseObj.setPayload(postRepo.save(inputPost));
         return responseObj;
     }
+    
+    public ResponseService findPostByUserId(IdObjectEntity inputUserId) {
+        ResponseService responseObj = new ResponseService();
+        Optional<List<PostEntity>> userPostsOpt = postRepo.findByUserIdOrderByCreatedAtDesc(inputUserId.getId());
+        if (userPostsOpt.isEmpty()) {
+            responseObj.setStatus("fail");
+            responseObj.setMessage("cannot find any post from user id: " + inputUserId.getId());
+            responseObj.setPayload(null);
+            return responseObj;
+        } else {
+            List<PostEntity> userPosts = userPostsOpt.get();
+            responseObj.setStatus("success");
+            responseObj.setMessage("success");
+            responseObj.setPayload(userPosts);
+            return responseObj;
+        }
+    }
+    public ResponseService updatePostByComment(PostEntity inputPost) {
+        ResponseService responseObj = new ResponseService();
+        Optional<PostEntity> optPost = postRepo.findById(inputPost.getId());
+        if (optPost.isEmpty()) {
+            responseObj.setStatus("fail");
+            responseObj.setMessage("cannot find post id: " + inputPost.getId());
+            responseObj.setPayload(null);
+            return responseObj;
+        } else {
+            // inputPost.setCreatedAt(Instant.now());
+            postRepo.save(inputPost);
+            responseObj.setStatus("success");
+            responseObj.setMessage("post is updated successfully");
+            responseObj.setPayload(inputPost);
+            return responseObj;
+        }
+    }
+    public ResponseService updatePostByLove(DoubleIdObjectEntity doubleId) {
+        // id 1 - post Id, id 2 - user who liked post
+        ResponseService responseObj = new ResponseService();
+        Optional<PostEntity> optPost = postRepo.findById(doubleId.getId1());
+        if (optPost.isEmpty()) {
+            responseObj.setStatus("fail");
+            responseObj.setMessage("cannot find post id: " + doubleId.getId1());
+            responseObj.setPayload(null);
+            return responseObj;
+        } else {
+            PostEntity targetPost = optPost.get();
+            List<String> loveList = targetPost.getLove();
+            if (loveList == null) {
+                loveList = new ArrayList<>();
+            }
+            // love and unlove a post
+            if (!loveList.contains(doubleId.getId2())) {
+                loveList.add(doubleId.getId2());
+            } else {
+                loveList.remove(doubleId.getId2());
+            }
+            targetPost.setLove(loveList);
+            postRepo.save(targetPost);
+            responseObj.setStatus("success");
+            responseObj.setMessage("update love to the target post id: " + targetPost.getId());
+            responseObj.setPayload(targetPost);
+            return responseObj;
+        }
+    }
+    public ResponseService updatePostByShare(DoubleIdObjectEntity doubleId) {
+        // id 1 - post Id, id 2 - user who shared post
+        ResponseService responseObj = new ResponseService();
+        Optional<PostEntity> optPost = postRepo.findById(doubleId.getId1());
+        if (optPost.isEmpty()) {
+            responseObj.setStatus("fail");
+            responseObj.setMessage("cannot find post id: " + doubleId.getId1());
+            responseObj.setPayload(null);
+            return responseObj;
+        } else {
+            PostEntity targetPost = optPost.get();
+            List<String> shareList = targetPost.getShare();
+            if (shareList == null) {
+                shareList = new ArrayList<>();
+            }
+            // save id of user who shared the post then update post
+            shareList.add(doubleId.getId2());
+            targetPost.setShare(shareList);
+            postRepo.save(targetPost);
+            // update post list of user who shared the post
+            targetPost.setUserId(doubleId.getId2());
+            targetPost.setId(null);
+            targetPost.setContent("Shared a post: " + targetPost.getContent());
+            targetPost.setLove(new ArrayList<>());
+            targetPost.setShare(new ArrayList<>());
+            targetPost.setComment(new ArrayList<>());
+            postRepo.save(targetPost);
 
+            responseObj.setStatus("success");
+            responseObj.setMessage("add a share to the target post id: " + targetPost.getId());
+            responseObj.setPayload(targetPost);
+            return responseObj;
+        }
+    }
 }
