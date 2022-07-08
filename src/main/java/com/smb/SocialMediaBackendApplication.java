2f8531e3c641715e4245f1bb2ef4665eaf4b6317
VipinKumar-07 <vipinkashyap10112000@gmail.com>
2022-07-08 12:24:22 +0530

    Cors Property added

--- src/main/java/com/smb/SocialMediaBackendApplication.java
+++ src/main/java/com/smb/SocialMediaBackendApplication.java
@@ -2,6 +2,9 @@ package com.smb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SocialMediaBackendApplication {
@@ -9,5 +12,17 @@ public class SocialMediaBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SocialMediaBackendApplication.class, args); System.out.println("hi");
 	}
+	
+	@Bean
+	public WebMvcConfigurer configure(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry reg) {
+				reg.addMapping("/**").allowedOrigins("*");
+			}
+		};
+		
+	}
+	
 
 }
