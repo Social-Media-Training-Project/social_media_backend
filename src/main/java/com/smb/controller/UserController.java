92838870a5297369174a2bd4299114abd5f45837
VipinKumar-07 <vipinkashyap10112000@gmail.com>
2022-07-08 15:53:16 +0530

    cors error resolve

--- src/main/java/com/smb/controller/UserController.java
+++ src/main/java/com/smb/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,7 @@ import com.smb.service.UserService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class UserController {
 	@Autowired
     private UserService userService;
